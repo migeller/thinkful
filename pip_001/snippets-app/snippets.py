7b6d1d4eaa5f3d75ef782409b23ffcb0e2fff94c
Matt Geller <matt@mmct.com>
2014-09-07 11:32:24 +0100

    main body created

--- pip_001/snippets-app/snippets.py
+++ pip_001/snippets-app/snippets.py
@@ -1,16 +1,31 @@
-import logging
-import csv
+import logging, csv, argparse, sys
 
 # Set the log output file, and the log level
-logging.basicConfig(filename="output.log", level=logging.DEBUG)
+logging.basicConfig(filename="output.log", level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
 
 def put(name, snippet, filename):
     """ Store a snippet with an associated name in the CSV file """
     logging.info("Writing {}:{} to {}".format(name, snippet, filename))
     logging.debug("Opening file")
     with open(filename, "a") as f:
-    	writer = csv.writer(f)
-    	logging.debug("Writing snippet to file".format(name, snippet))
-    	writer.writerow([name, snippet])
+        writer = csv.writer(f)
+        logging.debug("Writing snippet to file".format(name, snippet))
+        writer.writerow([name, snippet])
     logging.debug("Write successful")
     return name, snippet
+
+def make_parser():
+    """ Construct the command line parser """
+    logging.info("Constructing parser")
+    description = "Store and retrieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+    return parser
+
+def main():
+    """ Main function """
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
